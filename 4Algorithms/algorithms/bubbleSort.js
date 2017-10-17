c90205c3c6eddd46a755b9ae940bd106c01d1a8d
Azmeer Siddiq <reemza77@gmail.com>
2017-10-17 17:08:29 +0800

    bubblesort updated

--- 4Algorithms/algorithms/bubbleSort.js
+++ 4Algorithms/algorithms/bubbleSort.js
@@ -2,12 +2,12 @@
   A JavaScript module which performs a bubble sort on a given array of numbers, ordering the elements from lowest to highest.
 */
 module.exports = exports = function(input) {
-var z = 0
-var swi = false
+
 do{
+  var swi = false
   for (var i = 0; i< input.length; i++){
     if (input[i]> input[i+1]){
-      z = input[i]
+      var z = input[i]
       input[i] = input[i+1]
       input[i+1] = z
       swi = true
