6861b6df2cc5b3c67ddac20c806405aa2860d036
Azmeer Siddiq <reemza77@gmail.com>
2017-10-17 18:17:11 +0800

    corrected linearSearch

--- 4Algorithms/algorithms/linearSearch.js
+++ 4Algorithms/algorithms/linearSearch.js
@@ -2,12 +2,19 @@
   A JavaScript module which performs a linear search for a given value inside an input array.  If the specified value is found, the index of the element in the array with that value is returned.  If the specified value is not found in the array -1 is returned.
 */
 module.exports = exports = function(input, value) {
-
-for (var i = 0; i<input.length; i++){
-  if (input[i].toString() === value.toString()){
-    return i
+  if (value){
+    for (var k = 0; k<input.length; k++){
+      if (value.toString() === input[k].toString()){
+        return k
+      }
+    }
+  }
+  else{
+    for (var j = 0; j<input.length; j++){
+      if (value === input[j]){
+        return j
+      }
+    }
   }
   return -1
-}
-
 };
